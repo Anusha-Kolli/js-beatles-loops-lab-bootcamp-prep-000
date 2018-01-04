e3c2b01241447feeba83edd585e18bf8922a52a1
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-04 21:03:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function theBeatlesPlay(musicians,instruments){
   var phrases = [];
+  for( var i =0 ; i< musicians.length; i++){
+     phrases.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return phrases;
 
 }
